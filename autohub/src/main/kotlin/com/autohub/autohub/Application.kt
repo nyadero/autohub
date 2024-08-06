427ab6c9454db7d62d3a326081ab7d6faf0f06ac
nyadero <briannyadero443@gmail.com>
2024-08-06 19:33:04 +0300

    load secrets from dotenv

--- autohub/src/main/kotlin/com/autohub/autohub/Application.kt
+++ autohub/src/main/kotlin/com/autohub/autohub/Application.kt
@@ -14,10 +14,14 @@ fun main(args: Array<String>) {
     val dotEnv: Dotenv = Dotenv.load()
     // datasource
     System.setProperty("SPRING_DATASOURCE_URL", Objects.requireNonNull(dotEnv.get("SPRING_DATASOURCE_URL")))
-    System.setProperty("SPRING_DATASOURCE_USERNAME", dotEnv.get("SPRING_DATASOURCE_USERNAME"))
+    System.setProperty("SPRING_DATASOURCE_USERNAME", Objects.requireNonNull(dotEnv.get("SPRING_DATASOURCE_USERNAME")))
     System.setProperty("SPRING_DATASOURCE_PASSWORD", dotEnv.get("SPRING_DATASOURCE_PASSWORD"))
     System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotEnv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"))
 
+    // security
+    System.setProperty("JWT_SECRET_KEY", Objects.requireNonNull(dotEnv.get("JWT_SECRET_KEY")))
+    System.setProperty("JWT_EXPIRATION", Objects.requireNonNull(dotEnv.get("JWT_EXPIRATION")))
+    System.setProperty("JWT_REFRESH_TOKEN_EXPIRATION", Objects.requireNonNull("JWT_REFRESH_TOKEN_EXPIRATION"))
     runApplication<AutohubApplication>(*args)
 
 }
