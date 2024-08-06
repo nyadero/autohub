d2be0057f3cc107becb87552ba157df75c00b185
nyadero <briannyadero443@gmail.com>
2024-08-06 19:34:10 +0300

    users, verification_tokens and password_reset_token tables

---
+++ autohub/src/main/resources/db/migration/V2__create_auth_tables.sql
@@ -0,0 +1,33 @@
+create table if not exists users(
+  id varchar(255) not null ,
+  name  varchar(255) not null unique,
+  email  varchar(255) not null unique,
+  u_password varchar(255) not null unique,
+  is_enabled boolean default false,
+  role varchar(255) default USER,
+  created_at timestamp(6),
+  updated_at timestamp(6),
+  primary key(id)
+);
+
+-- password reset token table
+create table if not exists password_reset_tokens(
+  id varchar(255) not null,
+  token varchar(255) not null,
+  expires_in timestamp(6),
+  user_id varchar(255) not null unique,
+  created_at timestamp(6),
+  updated_at timestamp(6),
+  primary key(id)
+);
+
+-- verification token
+create table if not exists verification_tokens(
+  id varchar(255) not null,
+  token varchar(255) not null,
+  expires_in timestamp(6),
+  user_id varchar(255) not null unique,
+  created_at timestamp(6),
+  updated_at timestamp(6),
+  primary key(id)
+);
