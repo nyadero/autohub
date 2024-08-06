3c3741f7c6dfb56edd47f6cd1af17ef858618e44
nyadero <briannyadero443@gmail.com>
2024-08-06 22:53:33 +0300

    websecurity config

---
+++ autohub/src/main/kotlin/com/autohub/autohub/configuration/security/WebSecurityConfig.kt
@@ -0,0 +1,39 @@
+package com.autohub.autohub.configuration.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class WebSecurityConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder{
+        return BCryptPasswordEncoder(10)
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/api/v1/vehicles/**").permitAll()
+                    .requestMatchers("/api/v1/comments/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .csrf { csrf -> csrf.disable() }
+
+        return http.build()
+    }
+}
